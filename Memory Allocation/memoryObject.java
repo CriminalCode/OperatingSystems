9255644f9b14e47cbf15b47b2da818e2e41a7712
Daniel Heinen <dh9621@mcla.edu>
2017-04-13 16:15:06 +0000

    lol

---
+++ Memory Allocation/memoryObject.java
@@ -0,0 +1,20 @@
+public class memoryObject
+{
+    int start = 0;
+    int size = 0;
+    String holeOrnot = "Used";
+    
+    public memoryObject(int strt, int si, String holeorother)
+    {
+        start = strt;
+        size = si;
+        holeOrnot = holeorother;
+    }
+
+
+    public String toString()
+    {
+        return String.format("%d %d %d %s", start, size, id, holeOrnot);
+    }
+    
+}
